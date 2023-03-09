8a71670e65c8951ba4bfed93a4ef584c835d7150
Rakul <rabrosrajgopu@gmail.com>
2023-03-09 14:25:13 +0100

    added new navbar

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -12,7 +12,23 @@
 
 </style>
 
+
+
 <body>
+<header>
+    <a href="" class="logo"><img src="/images/logo.png" alt=""></a>
+    <ul class="navbar">
+        <li><a href="">Tutoring Uni</a></li>
+        <li><a href="">Kurse suchen</a></li>
+        <li><a href="">Über Uns</a></li>
+    </ul>
+
+    <div class="main">
+        <a href="">Sign In</a>
+        <a href="/register"><button class="buttonregister">Sign Up</button> </a>
+    </div>
+</header>
+
     <div class="container">
         <div class="title">
             <h1>Unser Team</h1>
@@ -82,12 +98,72 @@
             padding: 0;
             font-family: "Poppins", sans-serif;
             margin: 0;
+            text-decoration: none;
+            list-style: none;
             
         }
 
         body{
             background-color: #d6edd4;
         }
+
+        header{
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            z-index: 1000;
+            padding: 1px 0;
+
+        }
+
+        .logo{
+            display: flex;
+            align-items: center;
+        }
+
+        .navbar{
+            display: flex;
+            
+            
+        }
+
+        .navbar a{
+            padding: 5px 0;
+            margin: 10px 30px;
+            font-size: 18px;
+            font-weight: bold;
+            color: black;
+
+        }
+
+        .main{
+            display: flex;
+            align-items: center;
+        }
+
+        .main a{
+            margin-right: 45px;
+            margin-left: 40px;
+            font-weight: 500;
+            font-size: 18px;
+            font-weight: bold;
+            color: black;
+
+        }
+
+        .buttonregister{
+            width: 120px;
+            height: 40px;
+            border-radius: 25px; 
+            border: 1px solid black ;
+            background-color: #00B0FF;
+            font-size: 18px;
+            cursor: pointer;
+        
+        }
+
+       
         .container {
             text-align: center;
             
