f2b853bdb7e36ce266cd21d5f7d8aced761e892a
Rakul <rabrosrajgopu@gmail.com>
2023-04-06 13:13:52 +0200

    made profile page responsive

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,42 +7,43 @@
         <h1>Mein Profil</h1>
     </div>
     <div class="profile">
-    <div class="profile-info">
-        <img src="/images/pb.svg">
-        <p><strong>Vorname:</strong> {{$data->firstname}}</p>
-        <p><strong>Nachname:</strong> {{$data->lastname}}</p>
-        <p><strong>Email:</strong> {{$data->email}}</p>
-        <div class="logout-link">
-        <p><a href="logout">Logout</a></p>
-    </div>
-    </div>
-    <div class="profile-update">
-        <form action="" method="post">
-            @csrf
-
-            <div class="inputarea">
-                <input type="text" id="firstname" name="firstname" value="{{$data->firstname}}" placeholder="Vorname">
+        <div class="profile-info">
+            <img src="/images/pb.svg">
+            <p><strong>Vorname:</strong> {{$data->firstname}}</p>
+            <p><strong>Nachname:</strong> {{$data->lastname}}</p>
+            <p><strong>Email:</strong> {{$data->email}}</p>
+            <div class="logout-link">
+                <a href="logout">Logout</a>
             </div>
+        </div>
+        <div class="profile-update">
+            <form action="" method="post">
+                @csrf
 
-            <div class="inputarea">
-                <input type="text" id="lastname" name="lastname" value="{{$data->lastname}}" placeholder="Nachname">
-            </div>
+                <div class="inputarea">
+                    <input type="text" id="firstname" name="firstname" value="{{$data->firstname}}"
+                        placeholder="Vorname">
+                </div>
 
-            <div class="inputarea">
-                <input type="email" id="email" name="email" value="{{$data->email}}" placeholder="Email">
-            </div>
+                <div class="inputarea">
+                    <input type="text" id="lastname" name="lastname" value="{{$data->lastname}}" placeholder="Nachname">
+                </div>
 
-            <div class="inputarea">
-                <input type="email" id="email" name="new_email"  placeholder="Neue Email">
-            </div>
+                <div class="inputarea">
+                    <input type="email" id="email" name="email" value="{{$data->email}}" placeholder="Email">
+                </div>
 
-            <button type="submit" name="submit">Bearbeiten</button>
+                <div class="inputarea">
+                    <input type="email" id="email" name="new_email" placeholder="Neue Email">
+                </div>
 
-        </form>
+                <button type="submit" name="submit">Bearbeiten</button>
+
+            </form>
+        </div>
+
     </div>
 
-    </div>
-    
 </section>
 
 @endsection
