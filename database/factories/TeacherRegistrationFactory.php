49cf956b712797f18faf74169ed13731dfac4abb
Ben Streich <ben.streich@icloud.com>
2023-05-04 12:43:51 +0200

    Seeder added😈😈

--- database/factories/TeacherRegistrationFactory.php
+++ database/factories/TeacherRegistrationFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Registration>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TeacherRegistration>
  */
-class RegistrationFactory extends Factory
+class TeacherRegistrationFactory extends Factory
 {
     /**
      * Define the model's default state.
