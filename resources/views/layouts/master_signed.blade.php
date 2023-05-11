4e2abee7fb3513436d3cd039892cbb319e641507
Rakul <rabrosrajgopu@gmail.com>
2023-05-11 08:43:55 +0200

    minor changes

--- resources/views/layouts/master_signed.blade.php
+++ resources/views/layouts/master_signed.blade.php
@@ -53,7 +53,7 @@
             <div class="links">
                 <h2>Links</h2>
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="/start">Home</a></li>
                     <li><a href="/courses">Kurse suchen</a></li>
                     <li><a href="/about_us">Über Uns</a></li>
                 </ul>
